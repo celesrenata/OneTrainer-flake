2473d8fd29f8cf066a1bc64c1c867c82ed56f83e
Celes Renata <celes@frameshift.net>
2025-11-15 20:28:51 -0800

    Add pyarrow to dask dependencies

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
             };
             dask = super.dask.overridePythonAttrs (oldAttrs: {
               doCheck = false;
-              propagatedBuildInputs = (oldAttrs.propagatedBuildInputs or []) ++ [ self.numpy self.pandas ];
+              propagatedBuildInputs = (oldAttrs.propagatedBuildInputs or []) ++ [ self.numpy self.pandas self.pyarrow ];
             });
           };
         };
